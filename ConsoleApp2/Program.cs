542f2a32a406acebf25fd46ca82b651c0bd448c6
Marina Panchenko <marishka100281@gmail.com>
2025-09-13 16:21:56 +0300

    Add Project 2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "John";
+            byte MyAge = 25;
+            bool HaveIApet=true;
+            double MyShoeSize = 37.5;
+
+
+            Console.WriteLine("My name is " + MyName );
+            Console.WriteLine("I am " + MyAge + " years old");
+            Console.WriteLine("My shoe size is " + MyShoeSize);
+            Console.WriteLine( "Do I have a pet? " + HaveIApet);
+        }
+    }
+}
